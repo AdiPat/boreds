0f8ce2a0d98242291c809a8321359011ee0a1954
Aditya Patange <adipat247360@gmail.com>
2020-11-01 12:13:31 +0530

    Put individual pages under pages folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { LoginPage } from "./login/LoginPage";
-import { HomePage } from "./home/HomePage";
+import { LoginPage } from "./pages/login/LoginPage";
+import { HomePage } from "./pages/home/HomePage";
 import { firebaseApp } from "./firebase/firebase";
 import "./App.css";
 
