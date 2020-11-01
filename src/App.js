5283141b06b7b4e0a9e6b3bacbc9c7377be686eb
Aditya Patange <adipat247360@gmail.com>
2020-11-01 12:24:55 +0530

    Add Signup Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { LoginPage } from "./pages/login/LoginPage";
 import { HomePage } from "./pages/home/HomePage";
+import { SignupPage } from "./pages/signup/SignupPage";
 import { firebaseApp } from "./firebase/firebase";
 import "./App.css";
 
@@ -13,6 +14,9 @@ function App() {
         </Route>
         <Route exact path="/login">
           <LoginPage />
+        </Route>
+        <Route exact path="/signup">
+          <SignupPage />
         </Route>
       </Switch>
     </Router>
---
+++ src/pages/signup/SignupPage.js
@@ -0,0 +1,12 @@
+import Container from "@material-ui/core/Container";
+
+function SignupPage() {
+  return (
+    <Container maxWidth="sm">
+      <h1>Signup Page</h1>
+      <input type="text" placeholder="Enter email" />
+    </Container>
+  );
+}
+
+export { SignupPage };
