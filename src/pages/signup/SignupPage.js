8c5855147bfd4f7260d67573376e23717802b781
Aditya Patange <adipat247360@gmail.com>
2020-11-01 20:42:25 +0530

    Improve Sign Up Page - Add password visibility icon - Add TextField for full name - Add material-ui icons package

--- src/pages/signup/SignupPage.js
+++ src/pages/signup/SignupPage.js
@@ -4,12 +4,18 @@ import Card from "@material-ui/core/Card";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
 import firebase from "firebase/app";
 import "firebase/auth";
 
 function SignupPage() {
+  const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleSubmit = (e) => {
     console.log(email, password);
@@ -43,7 +49,16 @@ function SignupPage() {
             <Grid item xs={12}>
               <TextField
                 style={{ width: "100%" }}
-                label="Email"
+                label="Enter full name"
+                type="text"
+                size="medium"
+                onChange={(e) => setFullName(e.target.value)}
+              ></TextField>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                style={{ width: "100%" }}
+                label="Enter email address"
                 type="text"
                 size="medium"
                 onChange={(e) => setEmail(e.target.value)}
@@ -52,10 +67,23 @@ function SignupPage() {
             <Grid item xs={12}>
               <TextField
                 style={{ width: "100%" }}
-                label="Password"
-                type="password"
+                label="Create password"
+                type={showPassword ? "text" : "password"}
                 size="medium"
                 onChange={(e) => setPassword(e.target.value)}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={() => setShowPassword(!showPassword)}
+                        onMouseDown={() => setShowPassword(!showPassword)}
+                      >
+                        {showPassword ? <Visibility /> : <VisibilityOff />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
               ></TextField>
             </Grid>
             <Grid item xs={12}>
@@ -66,6 +94,13 @@ function SignupPage() {
                 onClick={handleSubmit}
               >
                 Sign Up
+              </Button>
+            </Grid>
+            <Grid item xs={12}>
+              <Button color="primary">
+                <a href="/login" style={{ textDecoration: "none" }}>
+                  Already have an account? Log in
+                </a>
               </Button>
             </Grid>
           </Grid>
