5033f172f7a9986908968ca22c6c192a36845c74
Aditya Patange <adipat247360@gmail.com>
2020-11-01 16:35:33 +0530

    Add Sign Up logic

--- src/pages/signup/SignupPage.js
+++ src/pages/signup/SignupPage.js
@@ -1,11 +1,77 @@
-import Container from "@material-ui/core/Container";
+import { useState, useEffect } from "react";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import firebase from "firebase/app";
+import "firebase/auth";
 
 function SignupPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    console.log(email, password);
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
+
   return (
-    <Container maxWidth="sm">
-      <h1>Signup Page</h1>
-      <input type="text" placeholder="Enter email" />
-    </Container>
+    <Grid
+      style={{ minHeight: "100vh" }}
+      container
+      direction="column"
+      justify="center"
+      alignItems="center"
+    >
+      <Grid item xs={12} md={4}>
+        <Card style={{ padding: "20px" }}>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <Typography align="center" variant="h6">
+                Sign Up for Boreds
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                style={{ width: "100%" }}
+                label="Email"
+                type="text"
+                size="medium"
+                onChange={(e) => setEmail(e.target.value)}
+              ></TextField>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                style={{ width: "100%" }}
+                label="Password"
+                type="password"
+                size="medium"
+                onChange={(e) => setPassword(e.target.value)}
+              ></TextField>
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                style={{ width: "100%" }}
+                variant="contained"
+                color="primary"
+                onClick={handleSubmit}
+              >
+                Sign Up
+              </Button>
+            </Grid>
+          </Grid>
+        </Card>
+      </Grid>
+    </Grid>
   );
 }
 
