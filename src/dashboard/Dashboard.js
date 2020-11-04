651efa35fd9519f699bcda9da666fa9219b96238
Aditya Patange <adipat247360@gmail.com>
2020-11-04 20:56:27 +0530

    Dashboard: Add content pane

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,12 +1,30 @@
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
-import Container from "@material-ui/core/Container";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import firebase from "firebase/app";
 import { DashDrawer } from "./DashDrawer";
 
+const useStyles = makeStyles((theme) => ({
+  toolbar: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(8),
+  },
+}));
+
 function Dashboard(props) {
   const history = useHistory();
+  const classes = useStyles();
+  const theme = useTheme();
   const [openDrawer, setOpenDrawer] = useState(false);
 
   const handleDrawerOpen = () => {
@@ -28,13 +46,20 @@ function Dashboard(props) {
         openDrawer={openDrawer}
         handleDrawerOpen={handleDrawerOpen}
         handleDrawerClose={handleDrawerClose}
+        handleLogout={handleLogout}
       ></DashDrawer>
-      <Container>
-        <h1>Welcome {props.user.email}</h1>
-        <Button type="contained" color="primary" onClick={handleLogout}>
-          Logout
-        </Button>
-      </Container>
+      <main className={classes.content}>
+        <Grid container style={{ marginLeft: "10px" }}>
+          <Grid item xs={12} className={classes.toolbar}>
+            <h1>Welcome {props.user.email}</h1>
+          </Grid>
+          <Grid item xs={12} className={classes.toolbar}>
+            <Button type="contained" color="primary" onClick={handleLogout}>
+              Logout
+            </Button>
+          </Grid>
+        </Grid>
+      </main>
     </div>
   );
 }
