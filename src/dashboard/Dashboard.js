b10ab5b73e577000de6de7cd608fd9b31770953a
Aditya Patange <adipat247360@gmail.com>
2020-11-04 16:32:18 +0530

    Dashboard: Add mini variant drawer

---
+++ src/dashboard/Dashboard.js
@@ -0,0 +1,150 @@
+import { useState } from "react";
+import clsx from "clsx";
+import { useHistory } from "react-router-dom";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
+import AppBar from "@material-ui/core/AppBar";
+import Drawer from "@material-ui/core/Drawer";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Divider from "@material-ui/core/Divider";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import firebase from "firebase/app";
+import App from "../App";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  hide: {
+    display: "none",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: "nowrap",
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: "hidden",
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9) + 1,
+    },
+  },
+  toolbar: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  },
+}));
+
+function Dashboard(props) {
+  const classes = useStyles();
+  const theme = useTheme();
+  const history = useHistory();
+  const [openDrawer, setOpenDrawer] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpenDrawer(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpenDrawer(false);
+  };
+
+  const handleLogout = () => {
+    firebase.auth().signOut();
+    history.push("/login");
+  };
+
+  return (
+    <div>
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: openDrawer,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, {
+              [classes.hide]: openDrawer,
+            })}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: openDrawer,
+          [classes.drawerClose]: !openDrawer,
+        })}
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: openDrawer,
+            [classes.drawerClose]: !openDrawer,
+          }),
+        }}
+      >
+        <div className={classes.toolbar}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "rtl" ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
+          </IconButton>
+        </div>
+        <Divider />
+      </Drawer>
+      <Container>
+        <h1>Welcome {props.user.email}</h1>
+        <Button type="contained" color="primary" onClick={handleLogout}>
+          Logout
+        </Button>
+      </Container>
+    </div>
+  );
+}
+
+export { Dashboard };
--- src/pages/DashPage.js
+++ src/pages/DashPage.js
@@ -1,31 +1,13 @@
 import { useContext, useEffect } from "react";
 import { useHistory, Redirect } from "react-router-dom";
-import Container from "@material-ui/core/Container";
-import Button from "@material-ui/core/Button";
 import UserContext from "../providers/UserContext";
-import firebase from "firebase/app";
+import { Dashboard } from "../dashboard/Dashboard";
 
 function DashPage() {
   const user = useContext(UserContext);
   const history = useHistory();
 
-  return user ? (
-    <Container>
-      <h1>Welcome {user.email}</h1>
-      <Button
-        type="contained"
-        color="primary"
-        onClick={() => {
-          firebase.auth().signOut();
-          history.push("/login");
-        }}
-      >
-        Logout
-      </Button>
-    </Container>
-  ) : (
-    <Redirect to="/login" />
-  );
+  return user ? <Dashboard user={user}></Dashboard> : <Redirect to="/login" />;
 }
 
 export { DashPage };
