d628ec8d4960588f3f9faf554f64844041cde166
Aditya Patange <adipat247360@gmail.com>
2020-11-06 19:58:27 +0530

    dashboard: Move out DashContent

---
+++ src/dashboard/DashContent.js
@@ -0,0 +1,44 @@
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import { BoardList } from "./BoardList";
+
+const useStyles = makeStyles((theme) => ({
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(8),
+    paddingRight: 0,
+  },
+}));
+
+function DashContent(props) {
+  const classes = useStyles();
+
+  return (
+    <main className={classes.content}>
+      <Grid container style={{ marginLeft: "10px", padding: "20px" }}>
+        <Grid item xs={12} spacing={2}>
+          <Typography variant="subtitle1">
+            Welcome {props.user.email}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Divider />
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        alignItems="center"
+        style={{ marginLeft: "10px", padding: "20px" }}
+      >
+        <Grid xs={12}>
+          <Typography variant="h4">Your Boards</Typography>
+        </Grid>
+        <BoardList boardsList={props.boardsList}></BoardList>
+      </Grid>
+    </main>
+  );
+}
+
+export { DashContent };
--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,33 +1,13 @@
 import { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
 import firebase from "firebase/app";
 import { boardsData } from "./boardsData";
 import { DashDrawer } from "./DashDrawer";
-import { BoardList } from "./BoardList";
-
-const useStyles = makeStyles((theme) => ({
-  toolbar: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "flex-end",
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(8),
-    paddingRight: 0,
-  },
-}));
+import { DashContent } from "./DashContent";
 
 function Dashboard(props) {
   const history = useHistory();
-  const classes = useStyles();
   const [openDrawer, setOpenDrawer] = useState(false);
   const [boardsList, setBoardsList] = useState(boardsData);
 
@@ -54,28 +34,10 @@ function Dashboard(props) {
         boardsList={boardsList}
         setBoardsList={setBoardsList}
       ></DashDrawer>
-      <main className={classes.content}>
-        <Grid container style={{ marginLeft: "10px", padding: "20px" }}>
-          <Grid item xs={12} spacing={2}>
-            <Typography variant="subtitle1">
-              Welcome {props.user.email}
-            </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Divider />
-          </Grid>
-        </Grid>
-        <Grid
-          container
-          alignItems="center"
-          style={{ marginLeft: "10px", padding: "20px" }}
-        >
-          <Grid xs={12}>
-            <Typography variant="h4">Your Boards</Typography>
-          </Grid>
-          <BoardList boardsList={boardsList}></BoardList>
-        </Grid>
-      </main>
+      <DashContent
+        user={props.user}
+        boardsList={props.boardsList}
+      ></DashContent>
     </div>
   );
 }
