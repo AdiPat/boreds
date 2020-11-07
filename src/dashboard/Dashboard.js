b29d65596744f37b30cc57623dd969b028d20a74
Aditya Patange <adipat247360@gmail.com>
2020-11-07 16:42:35 +0530

    refactor: Move DashDrawer and Modal to components/

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import firebase from "firebase/app";
-import { DashDrawer } from "./DashDrawer";
+import { DashDrawer } from "../components/DashDrawer";
 import { DashContent } from "./DashContent";
 
 function Dashboard(props) {
