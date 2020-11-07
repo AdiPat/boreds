76abf2fe0bdd81a1759307def7c188ddda777107
Aditya Patange <adipat247360@gmail.com>
2020-11-07 20:38:59 +0530

    BoardHome: Load board lists

--- src/board/BoardHome.js
+++ src/board/BoardHome.js
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import { DashDrawer } from "../components/DashDrawer";
 import { BoardContent } from "./BoardContent";
+import { sampleBoardLists } from "./sampleBoardLists";
 
 function BoardHome(props) {
+  const [boardLists, setBoardsList] = useState(sampleBoardLists);
+
   return (
     <div>
       <DashDrawer />
-      <BoardContent boardId={props.boardId} />
+      <BoardContent boardLists={boardLists} boardId={props.boardId} />
     </div>
   );
 }
