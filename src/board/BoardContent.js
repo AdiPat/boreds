0e8a5ac9100ab8f05c9f8a37792fde3701158cc7
Aditya Patange <adipat247360@gmail.com>
2020-11-07 20:51:50 +0530

    BoardContent: Fix spacing

--- src/board/BoardContent.js
+++ src/board/BoardContent.js
@@ -5,8 +5,10 @@ import Typography from "@material-ui/core/Typography";
 const useStyles = makeStyles((theme) => ({
   content: {
     flexGrow: 1,
-    padding: theme.spacing(8),
     paddingRight: 0,
+    paddingLeft: 0,
+    marginLeft: theme.spacing(10),
+    marginTop: theme.spacing(10),
   },
 }));
 
@@ -15,13 +17,7 @@ function BoardContent(props) {
 
   return (
     <main className={classes.content}>
-      <Grid container style={{ marginLeft: "10px", padding: "20px" }}>
-        <Grid item xs={12}>
-          <Typography variant="h3">
-            Welcome boardId = {props.boardId}
-          </Typography>
-        </Grid>
-      </Grid>
+      <h1>Sample Text</h1>
     </main>
   );
 }
