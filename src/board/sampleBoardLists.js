5da2a5115113af41036b84dfe1f875e22217c18d
Aditya Patange <adipat247360@gmail.com>
2020-11-07 20:33:25 +0530

    boards: Add dummy data

---
+++ src/board/sampleBoardLists.js
@@ -0,0 +1,24 @@
+const sampleBoardLists = [
+  {
+    id: "1a",
+    title: "Current",
+    cards: ["Task 1", "Task 2", "Task 3"],
+  },
+  {
+    id: "1b",
+    title: "Pending",
+    cards: [
+      "This is a test card",
+      "Work on ABC",
+      "Test data",
+      "This is another card",
+    ],
+  },
+  {
+    id: "2c",
+    title: "Next Week",
+    cards: ["Do dishes", "Pay bills", "Workout", "Call Sam"],
+  },
+];
+
+export { sampleBoardLists };
