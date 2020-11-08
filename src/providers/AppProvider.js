ac31e574387eed6f4754a8a388cb02370524ea3e
Aditya Patange <adipat247360@gmail.com>
2020-11-08 15:42:32 +0530

    AppProvider: Add listener to track changes to boards and update state

--- src/providers/AppProvider.js
+++ src/providers/AppProvider.js
@@ -1,14 +1,14 @@
 import React from "react";
 import firebase from "firebase/app";
+import "firebase/database";
 import AppContext from "./AppContext";
-import { boardsData } from "../dashboard/boardsData";
 
 class AppProvider extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       user: null,
-      boardsList: boardsData,
+      boardsList: {},
     };
     this.setBoardsList = this.setBoardsList.bind(this);
   }
@@ -18,9 +18,16 @@ class AppProvider extends React.Component {
   }
 
   componentDidMount() {
+    const thisComponent = this;
     firebase.auth().onAuthStateChanged((userAuth) => {
       console.log("Auth state changed: ", userAuth);
       this.setState({ user: userAuth });
+
+      const boardsRef = firebase.database().ref(`users/${userAuth.uid}/boards`);
+      boardsRef.on("value", function (snapshot) {
+        const newBoardsList = snapshot.val();
+        thisComponent.setBoardsList(newBoardsList);
+      });
     });
   }
 
