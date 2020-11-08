3074d1900d052b9e04aad30b97fa7d17693a87af
Aditya Patange <adipat247360@gmail.com>
2020-11-08 15:34:30 +0530

    user service: Add board functions

--- src/services/user.js
+++ src/services/user.js
@@ -1,5 +1,7 @@
 import firebase from "firebase/app";
+import uniqid from "uniqid";
 import "firebase/database";
+import "firebase/auth";
 
 const updateUserOnSignup = (userId, userObj) => {
   if (userObj !== null) {
@@ -37,4 +39,80 @@ const updateUserIfNotFound = (user) => {
     });
 };
 
-export { updateUserOnSignup, updateUserIfNotFound };
+// gets boards for user
+const getBoards = async (userId) => {
+  const userRef = firebase.database().ref(`users/${userId}`);
+  let boards = [];
+  try {
+    const userSnapshot = await userRef.once("value");
+    const user = userSnapshot.val();
+    boards = user.boards;
+    if (boards === undefined) {
+      boards = [];
+    }
+  } catch (err) {
+    console.error(`Failed to get boards for ${userId}`, err);
+  }
+  return boards;
+};
+
+// adds empty board
+const addNewBoard = async (userId, boardTitle) => {
+  const errMsg = "";
+  const boardId = uniqid();
+  const database = firebase.database();
+  const boardData = {
+    id: boardId,
+    title: boardTitle,
+    lanes: [
+      {
+        id: uniqid(),
+        title: "To Do",
+        cards: [
+          {
+            id: uniqid(),
+            title: "Eat breakfast",
+            description: "Make Milk and Cereal.",
+          },
+          {
+            id: uniqid(),
+            title: "Pay Bill",
+            description: "Electricity bill pending.",
+          },
+        ],
+      },
+      {
+        id: uniqid(),
+        title: "Completed",
+        cards: [
+          {
+            id: uniqid(),
+            title: "Data Science Assignment",
+            description: "Complete Linear Regression.",
+          },
+        ],
+      },
+    ],
+  };
+  database
+    .ref(`/users/${userId}/boards/${boardId}`)
+    .set(boardData)
+    .then(() => {
+      console.log("Successfully added board: ", boardTitle);
+    })
+    .catch((err) => {
+      console.error("Failed to add board.", err);
+    });
+};
+
+const getCurrentUser = () => {
+  return firebase.auth().currentUser;
+};
+
+export {
+  updateUserOnSignup,
+  updateUserIfNotFound,
+  getBoards,
+  getCurrentUser,
+  addNewBoard,
+};
