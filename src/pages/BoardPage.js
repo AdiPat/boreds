1605995073f7628b04226b7686c87a15b38ffda3
Aditya Patange <adipat247360@gmail.com>
2020-11-08 09:36:25 +0530

    BoardPage: Add redirect fix to BoardPage

--- src/pages/BoardPage.js
+++ src/pages/BoardPage.js
@@ -1,13 +1,27 @@
-import { useContext, useEffect } from "react";
+import { useState, useContext, useEffect } from "react";
 import { Redirect } from "react-router-dom";
 import AppContext from "../providers/AppContext";
 import { BoardHome } from "../board/BoardHome";
+import { CircularLoader } from "../components/CircularLoader";
 
 function BoardPage(props) {
   const { user } = useContext(AppContext);
+  const [isLoading, setIsLoading] = useState(true);
   const boardId = props.match.params.boardId;
 
-  return user ? <BoardHome boardId={boardId} /> : <Redirect to="/login" />;
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  });
+
+  return isLoading ? (
+    <CircularLoader color="secondary" />
+  ) : user ? (
+    <BoardHome boardId={boardId} />
+  ) : (
+    <Redirect to="/login" />
+  );
 }
 
 export { BoardPage };
