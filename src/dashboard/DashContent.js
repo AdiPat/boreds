57285a2ce743be48093270f43c32ca590617de27
Aditya Patange <adipat247360@gmail.com>
2020-11-12 14:31:19 +0530

    dashboard: Add create board card if there are no boards

--- src/dashboard/DashContent.js
+++ src/dashboard/DashContent.js
@@ -6,10 +6,19 @@ import Divider from "@material-ui/core/Divider";
 import LibraryBooksIcon from "@material-ui/icons/LibraryBooks";
 import StarIcon from "@material-ui/icons/Star";
 import ScheduleIcon from "@material-ui/icons/Schedule";
+import Button from "@material-ui/core/Button";
 import { BoardList } from "./BoardList";
 import { DashPane } from "./DashPane";
+import { NoBoardAction } from "./NoBoardAction";
 import AppContext from "../providers/AppContext";
 import { getStarredBoards, getRecentBoards } from "../services/board";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardActions,
+  CardActionArea,
+} from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   dashItem: {
@@ -50,21 +59,29 @@ function DashContent(props) {
           <Divider />
         </Grid>
       </Grid>
-      <DashPane
-        paneTitle="Starred Boards"
-        icon={<StarIcon />}
-        paneBoards={starredBoards}
-      />
-      <DashPane
-        paneTitle="Recent Boards"
-        icon={<ScheduleIcon />}
-        paneBoards={recentBoards}
-      />
-      <DashPane
-        paneTitle="All Boards"
-        icon={<LibraryBooksIcon />}
-        paneBoards={boardsList}
-      />
+      {starredBoards && starredBoards.length ? (
+        <DashPane
+          paneTitle="Starred Boards"
+          icon={<StarIcon />}
+          paneBoards={starredBoards}
+        />
+      ) : null}
+      {recentBoards && recentBoards.length ? (
+        <DashPane
+          paneTitle="Recent Boards"
+          icon={<ScheduleIcon />}
+          paneBoards={recentBoards}
+        />
+      ) : null}
+      {Object.keys(boardsList).length ? (
+        <DashPane
+          paneTitle="All Boards"
+          icon={<LibraryBooksIcon />}
+          paneBoards={boardsList}
+        />
+      ) : (
+        <NoBoardAction />
+      )}
     </main>
   );
 }
---
+++ src/dashboard/NoBoardAction.js
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import {
+  Grid,
+  Card,
+  CardContent,
+  CardActions,
+  Button,
+  Typography,
+} from "@material-ui/core";
+import { CircularLoader } from "../components/CircularLoader";
+import { CreateBoardModal } from "../components/CreateBoardModal";
+
+function NoBoardAction() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
+  useEffect(() => {
+    setTimeout(() => setIsLoaded(true), 500);
+  });
+
+  return !isLoaded ? (
+    <CircularLoader color="primary" loaderHeight="100px" />
+  ) : (
+    <Grid container style={{ marginLeft: "10px", padding: "20px" }}>
+      <Grid item xs={12} sm={2} md={2}>
+        <Card variant="outlined">
+          <CardContent>
+            <Typography variant="body1">You have no cards.</Typography>
+          </CardContent>
+          <CardActions>
+            <Button
+              variant="outlined"
+              color="primary"
+              onClick={handleOpenModal}
+            >
+              Create Board
+            </Button>
+          </CardActions>
+        </Card>
+      </Grid>
+      <CreateBoardModal
+        handleOpenModal={handleOpenModal}
+        handleCloseModal={handleCloseModal}
+        openModal={openModal}
+      />
+    </Grid>
+  );
+}
+
+export { NoBoardAction };
