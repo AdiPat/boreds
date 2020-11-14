0639bde03697b259488383da2e24dd10f9cb8ab9
Aditya Patange <adipat247360@gmail.com>
2020-11-14 13:25:19 +0530

    invite: Check duplicate before sending invite

--- src/services/invite.js
+++ src/services/invite.js
@@ -4,12 +4,29 @@ import "firebase/database";
 const createInvite = (fromEmail, toEmail, boardId) => {
   const database = firebase.database();
   const inviteRef = database.ref("/invites");
-  const newInviteRef = inviteRef.push();
-  newInviteRef.set({
-    from: fromEmail,
-    to: toEmail,
-    boardId: boardId,
-  });
+  let foundDuplicate = false;
+  inviteRef
+    .orderByChild("to")
+    .equalTo(toEmail)
+    .once("value", function (snapshot) {
+      const foundInvites = snapshot.val();
+      if (foundInvites) {
+        Object.keys(foundInvites).forEach((inviteKey) => {
+          const curInvite = foundInvites[inviteKey];
+          if (curInvite.boardId == boardId) {
+            foundDuplicate = true;
+          }
+        });
+      }
+      if (!foundDuplicate) {
+        const newInviteRef = inviteRef.push();
+        newInviteRef.set({
+          from: fromEmail,
+          to: toEmail,
+          boardId: boardId,
+        });
+      }
+    });
 };
 
 export { createInvite };
