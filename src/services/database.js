c9cff915919ef59e18883fc86197b1dd56c36fa1
Aditya Patange <adipat247360@gmail.com>
2020-11-15 07:32:56 +0530

    database service: Attach listener for invites

--- src/services/database.js
+++ src/services/database.js
@@ -1,5 +1,6 @@
 import firebase from "firebase";
 import "firebase/database";
+import { getBoardTitle } from "./board";
 
 const attachBoardUpdateListener = (boardId, updateBoardInState) => {
   const database = firebase.database();
@@ -40,8 +41,44 @@ const attachBoardAddedListener = (userId, updateBoardInState) => {
   });
 };
 
+const attachInvitesListener = async (userId, updateInvitesInState) => {
+  const database = firebase.database();
+  const userEmailRef = database.ref(`/users/${userId}/email`);
+  const email = await (await userEmailRef.once("value")).val();
+  try {
+    const invitesRef = database.ref("/invites");
+    invitesRef
+      .orderByChild("to")
+      .equalTo(email)
+      .on("value", async function (inviteSnapshot) {
+        console.log("Invites value changed listener called.");
+        let inviteNotifications = [];
+        let invites = inviteSnapshot.val();
+
+        if (invites) {
+          for (let inviteKey in invites) {
+            let inviteObj = invites[inviteKey];
+            const boardId = inviteObj.boardId;
+            const boardTitle = await getBoardTitle(boardId);
+            let notificationData = Object.assign({}, inviteObj);
+            notificationData.boardTitle = boardTitle;
+            inviteNotifications.push(notificationData);
+          }
+
+          updateInvitesInState(inviteNotifications);
+        }
+      });
+  } catch (err) {
+    console.error(
+      "attachInviteListener: Failed to get invite notifications. ",
+      err
+    );
+  }
+};
+
 export {
   attachBoardUpdateListener,
   attachBoardDeleteListener,
   attachBoardAddedListener,
+  attachInvitesListener,
 };
