89f8a9b869d28bba1ef0bb8e8cf69dcc7ed16027
Aditya Patange <adipat247360@gmail.com>
2020-11-22 12:41:09 +0530

    functions board: Add read write permissions on board creation

--- functions/board/index.js
+++ functions/board/index.js
@@ -64,24 +64,47 @@ exports.updateBoardUserOnCreation = functions.database
     const database = admin.database();
     const userId = context.params.userId;
     const boardId = context.params.boardId;
-    const boardRef = database.ref(`boards/${boardId}`);
+    const boardRef = database.ref(`boards/${boardId}/permissions`);
     const userEmailRef = database.ref(`users/${userId}/email`);
     const userEmail = await (await userEmailRef.once("value")).val();
-    return boardRef
-      .update({ users: [userEmail] })
+
+    const readUpdate = boardRef
+      .child("read")
+      .push(userEmail)
       .then(() => {
         console.log(
-          `Successfully added to list of users for `,
+          `Successfully updated read permissions for ${userEmail} and ${boardId}`,
           boardRef.toString()
         );
       })
       .catch((err) => {
         if (err) {
           console.error(
-            `Failed to add to list of users in: `,
+            `Failed to update read permissions for ${userEmail} and ${boardId}`,
             boardRef.toString()
           );
         }
         return false;
       });
+
+    const writeUpdate = boardRef
+      .child("write")
+      .push(userEmail)
+      .then(() => {
+        console.log(
+          `Successfully updated write permissions for ${userEmail} and ${boardId}`,
+          boardRef.toString()
+        );
+      })
+      .catch((err) => {
+        if (err) {
+          console.error(
+            `Failed to update write permissions for ${userEmail} and ${boardId}`,
+            boardRef.toString()
+          );
+        }
+        return false;
+      });
+
+    return Promise.all([readUpdate, writeUpdate]);
   });
