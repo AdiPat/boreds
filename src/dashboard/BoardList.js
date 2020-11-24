1cf93bee5b76cad0ea1d23d7b7dbd6b765edae25
Aditya Patange <adipat247360@gmail.com>
2020-11-24 17:28:06 +0530

    BoardList: Remove verbose and unused imports

--- src/dashboard/BoardList.js
+++ src/dashboard/BoardList.js
@@ -1,13 +1,8 @@
-import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { BoardListItem } from "./BoardListItem";
-import { DeleteBoardModal } from "../components/DeleteBoardModal";
-import { getBoards, getCurrentUser } from "../services/user";
 
 function BoardList(props) {
   const boardsList = props.boardsList;
-
-  console.log("BoardList: ", boardsList);
 
   const history = useHistory();
   const boardListComponent = Object.keys(boardsList).map((objId) => {
