b9aef9d319078f865aca3570150cd383bf1d4bd6
Aditya Patange <adipat247360@gmail.com>
2020-11-26 15:58:38 +0530

    CircularLoader: Add prop types

--- src/components/CircularLoader.js
+++ src/components/CircularLoader.js
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
@@ -22,5 +23,14 @@ function CircularLoader(props) {
     </div>
   );
 }
+
+CircularLoader.propTypes = {
+  loaderHeight: PropTypes.number,
+  color: PropTypes.string.isRequired,
+};
+
+CircularLoader.defaultProps = {
+  color: "secondary",
+};
 
 export { CircularLoader };
