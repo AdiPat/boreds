b160fc147ea587145ae89f830b8221021113bcf7
Aditya Patange <adipat247360@gmail.com>
2020-11-26 23:13:10 +0530

    functions tasks: deleteTaskTrigger()

--- functions/tasks/index.js
+++ functions/tasks/index.js
@@ -1,7 +1,9 @@
 const addTask = require("./addTask");
 const addTaskTrigger = require("./addTaskTrigger");
 const deleteTask = require("./deleteTask");
+const deleteTaskTrigger = require("./deleteTaskTrigger");
 
 exports.addTask = addTask.func;
 exports.deleteTask = deleteTask.func;
 exports.addTaskTrigger = addTaskTrigger.func;
+exports.deleteTaskTrigger = deleteTaskTrigger.func;
