860aaf773402e31d98e6ea6963ab0dfd59ce08d3
Aditya Patange <adipat247360@gmail.com>
2020-11-26 14:05:07 +0530

    Revert "BoardPage: Add board resource check"
    This reverts commit 991b5b6985baa3b94dd571036aef99b4964e2d3e.

--- src/pages/BoardPage.js
+++ src/pages/BoardPage.js
@@ -1,11 +1,8 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import AppContext from "../providers/AppContext";
 import { BoardHome } from "../board/BoardHome";
-import { NotFoundPage } from "./NotFoundPage";
-import { boardExists } from "../services/board";
 
 function BoardPage(props) {
-  const [resourceExists, setResourceExists] = useState(false);
   const { user } = useContext(AppContext);
   const boardId = props.match.params.boardId;
   let userId = null;
@@ -14,16 +11,7 @@ function BoardPage(props) {
     userId = user.uid;
   }
 
-  useEffect(() => {
-    console.log("resourceExists: ", resourceExists);
-    boardExists(boardId).then((status) => setResourceExists(status));
-  }, []);
-
-  return resourceExists ? (
-    <BoardHome boardId={boardId} userId={userId} />
-  ) : (
-    <NotFoundPage />
-  );
+  return <BoardHome boardId={boardId} userId={userId} />;
 }
 
 export { BoardPage };
--- src/pages/NotFoundPage.js
+++
@@ -1,9 +0,0 @@
-function NotFoundPage() {
-  return (
-    <div>
-      <h1>Resource not found. </h1>
-    </div>
-  );
-}
-
-export { NotFoundPage };
