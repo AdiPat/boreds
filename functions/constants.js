044981b68512ed743e75f5d5e536d6ee1bf2c6c6
Aditya Patange <adipat247360@gmail.com>
2020-11-26 23:01:41 +0530

    functions tasks: addTaskTrigger()

--- functions/constants.js
+++ functions/constants.js
@@ -2,4 +2,7 @@ exports.constants = {
   MAX_BOARD_TITLE_LENGTH: 200,
   MAX_TASK_TITLE_LENGTH: 200,
   MAX_TASK_DESCRIPTION_LENGTH: 1000,
+  ROLES: {
+    admin: "admin",
+  },
 };
--- functions/tasks/index.js
+++ functions/tasks/index.js
@@ -1,5 +1,7 @@
 const addTask = require("./addTask");
+const addTaskTrigger = require("./addTaskTrigger");
 const deleteTask = require("./deleteTask");
 
 exports.addTask = addTask.func;
 exports.deleteTask = deleteTask.func;
+exports.addTaskTrigger = addTaskTrigger.func;
