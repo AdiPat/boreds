d9b9c403ae7eef7479b79d72a5cd816b99f4e494
Aditya Patange <adipat247360@gmail.com>
2020-11-30 20:04:57 +0530

    ActivityInputField: Call service and some ui tweaks

--- src/tasks/ActivityInputField.js
+++ src/tasks/ActivityInputField.js
@@ -1,55 +1,93 @@
-import { Grid, TextField, IconButton, InputProps } from "@material-ui/core";
+import { useState } from "react";
+import PropTypes from "prop-types";
+import {
+  Grid,
+  TextField,
+  IconButton,
+  InputProps,
+  Snackbar,
+} from "@material-ui/core";
 import { lightGreen } from "@material-ui/core/colors";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import AddBoxIcon from "@material-ui/icons/AddBox";
+import { addTaskActivity } from "../services/tasks-activity";
 
 const useStyles = makeStyles((theme) => ({
   inputContainer: {
-    display: "flex",
-    flexDirection: "row",
-    flexGrow: 1,
-    paddingRight: 0,
     paddingLeft: theme.spacing(2),
-    marginLeft: theme.spacing(11),
-    marginTop: theme.spacing(2),
     alignItems: "flex-start",
-    ["@media (max-width: 400px)"]: {
-      marginLeft: theme.spacing(7),
-    },
   },
 }));
 
-function ActivityInputField() {
+function ActivityInputField(props) {
   const classes = useStyles();
+  const [activityText, setActivityText] = useState("");
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+
   const theme = useTheme();
 
+  const handleTextChange = (e) => {
+    setActivityText(e.target.value);
+  };
+
+  const handleActivitySubmit = () => {
+    // TODO: Figure out how to add position (last parameter)
+    addTaskActivity(props.taskId, activityText, 0).then((result) => {
+      if (!result.status && result.message) {
+        setOpenSnackbar(true);
+        setSnackbarMessage(result.message);
+      }
+    });
+  };
+
   return (
-    <div className={classes.inputContainer}>
-      <Grid container>
-        <Grid item md={6} sm={8} xs={10}>
-          <TextField
-            label="Enter Task Activity"
-            variant="outlined"
-            style={{ width: "100%" }}
-            InputProps={{
-              endAdornment: (
-                <IconButton size="medium" disableFocusRipple>
-                  <AddBoxIcon
-                    size="medium"
-                    style={{
-                      height: "40px",
-                      width: "40px",
-                      color: lightGreen[700],
-                    }}
-                  />
-                </IconButton>
-              ),
-            }}
-          />
-        </Grid>
+    <Grid container className={classes.inputContainer}>
+      <Grid item md={6} sm={8} xs={10}>
+        <TextField
+          label="Enter Task Activity"
+          variant="outlined"
+          style={{ width: "100%" }}
+          value={activityText}
+          onChange={handleTextChange}
+          InputProps={{
+            endAdornment: (
+              <IconButton
+                size="medium"
+                disableFocusRipple
+                onClick={handleActivitySubmit}
+              >
+                <AddBoxIcon
+                  size="medium"
+                  style={{
+                    height: "40px",
+                    width: "40px",
+                    color: lightGreen[700],
+                  }}
+                />
+              </IconButton>
+            ),
+          }}
+        />
       </Grid>
-    </div>
+      <Snackbar
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        open={openSnackbar}
+        onClose={(event, reason) => {
+          setOpenSnackbar(false);
+        }}
+        autoHideDuration={3000}
+        message={snackbarMessage}
+      />
+    </Grid>
   );
 }
+
+ActivityInputField.propTypes = {
+  taskId: PropTypes.string.isRequired,
+};
 
 export { ActivityInputField };
