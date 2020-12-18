d0c13097017621f87bb0406d425f9d2df820318f
Aditya Patange <adipat247360@gmail.com>
2020-12-18 15:10:59 +0530

    CalendarHeader: Seperate selector button and header

--- src/calendar/CalendarHeaderItemBody.js
+++ src/calendar/CalendarHeaderItemBody.js
@@ -1,44 +1,18 @@
-import React, { useContext } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import moment from "moment";
 import clsx from "clsx";
-import { deepOrange, grey } from "@material-ui/core/colors";
-import { Button, Avatar } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import CalendarContext from "../providers/CalendarContext";
+import { SlotDateSelectButton } from "./buttons/SlotDateSelectButton";
 
 const useStyles = makeStyles((theme) => ({
   hide: {
     display: "none !important",
   },
-  selectDateBtn: {
-    "&:hover": {
-      backgroundColor: "white",
-    },
-  },
-  animate: {
-    transition: "all 0.3s linear",
-  },
-  avatarOrange: {
-    color: theme.palette.getContrastText(deepOrange[500]),
-    backgroundColor: deepOrange[500],
-  },
-  avatarWhite: {
-    color: "black",
-    backgroundColor: "white",
-    "&:hover": {
-      backgroundColor: grey[100],
-    },
-  },
 }));
 
 function CalendarHeaderItemBody({ date, isDuration, isDateSelected }) {
   const classes = useStyles();
-  const { setSelectedDate } = useContext(CalendarContext);
-
-  const handleSelectDate = () => {
-    setSelectedDate(date);
-  };
 
   return (
     <div
@@ -46,20 +20,7 @@ function CalendarHeaderItemBody({ date, isDuration, isDateSelected }) {
         [classes.hide]: isDuration.year || isDuration.month,
       })}
     >
-      <Button
-        className={classes.selectDateBtn}
-        onClick={handleSelectDate}
-        disableRipple
-      >
-        <Avatar
-          className={clsx(classes.animate, {
-            [classes.avatarOrange]: isDateSelected,
-            [classes.avatarWhite]: !isDateSelected,
-          })}
-        >
-          {date.format("DD")}
-        </Avatar>
-      </Button>
+      <SlotDateSelectButton date={date} isDateSelected={isDateSelected} />
     </div>
   );
 }
---
+++ src/calendar/buttons/SlotDateSelectButton.js
@@ -0,0 +1,67 @@
+import React, { useContext } from "react";
+import PropTypes from "prop-types";
+import moment from "moment";
+import clsx from "clsx";
+import { deepOrange, grey } from "@material-ui/core/colors";
+import { Button, Avatar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import CalendarContext from "../../providers/CalendarContext";
+
+const useStyles = makeStyles((theme) => ({
+  hide: {
+    display: "none !important",
+  },
+  selectDateBtn: {
+    "&:hover": {
+      backgroundColor: "white",
+    },
+  },
+  animateAvatar: {
+    transition: "all 0.3s linear",
+  },
+  avatarOrange: {
+    color: theme.palette.getContrastText(deepOrange[500]),
+    backgroundColor: deepOrange[500],
+  },
+  avatarWhite: {
+    color: "black",
+    backgroundColor: "white",
+    "&:hover": {
+      backgroundColor: grey[100],
+    },
+  },
+}));
+
+function SlotDateSelectButton({ date, isDateSelected }) {
+  const classes = useStyles();
+
+  const { setSelectedDate } = useContext(CalendarContext);
+
+  const handleSelectDate = () => {
+    setSelectedDate(date);
+  };
+
+  return (
+    <Button
+      className={classes.selectDateBtn}
+      onClick={handleSelectDate}
+      disableRipple
+    >
+      <Avatar
+        className={clsx(classes.animateAvatar, {
+          [classes.avatarOrange]: isDateSelected,
+          [classes.avatarWhite]: !isDateSelected,
+        })}
+      >
+        {date.format("DD")}
+      </Avatar>
+    </Button>
+  );
+}
+
+SlotDateSelectButton.propTypes = {
+  date: PropTypes.instanceOf(moment).isRequired,
+  isDateSelected: PropTypes.bool.isRequired,
+};
+
+export { SlotDateSelectButton };
